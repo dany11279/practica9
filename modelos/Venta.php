8999efe73f52491cf523ce7804c12623e05a9758
Dany <danymel1279@gmail.com>
2023-06-05 23:19:37 -0600

    termine de corregir mis errores

--- modelos/Venta.php
+++ modelos/Venta.php
@@ -23,7 +23,7 @@ class Venta extends Conexion{
     }
 
         public function buscar(){
-            $sql = "SELECT cliente_nombre, venta_fecha, producto_nombre, producto_precio, detalle_cantidad, (producto_precio * detalle_cantidad) as total from ventas inner join clientes on venta_cliente = cliente_id  
+            $sql = "SELECT detalle_id, cliente_nombre, venta_fecha, producto_nombre, producto_precio, detalle_cantidad, (producto_precio * detalle_cantidad) as total from ventas inner join clientes on venta_cliente = cliente_id  
              INNER JOIN detalle_ventas ON venta_id = detalle_venta
              INNER JOIN productos ON detalle_producto = producto_id
             where venta_situacion= 1 ";
@@ -43,5 +43,23 @@ class Venta extends Conexion{
         $resultado = self::servir($sql);
         return $resultado;
     }
+
+    
+    public function factura($id){
+        $sql = "SELECT detalle_id, cliente_nombre, venta_fecha, cliente_nit, producto_nombre, producto_precio, detalle_cantidad, (producto_precio * detalle_cantidad) as total from ventas inner join clientes on venta_cliente = cliente_id  
+         INNER JOIN detalle_ventas ON venta_id = detalle_venta
+         INNER JOIN productos ON detalle_producto = producto_id
+        where venta_situacion= 1 and detalle_id= $id ";
+
+   
+
+
+    $resultado = self::servir($sql);
+    return $resultado;
+}
+
+    
+
+
 
 }
